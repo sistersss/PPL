a13aefe10d7f3b861da1787cdcc4bd64af3c5ace
TikaYulian27 <tikayulian27@gmail.com>
2019-11-04 00:37:36 +0700

    Menambahkan 5 test case

--- index.js
+++ index.js
@@ -15,7 +15,40 @@ describe('Testing', ()=>{
         tester.get('http://spmb.polinema.ac.id/')
     });
 
-    it('Accessing SPMB and Click Home button', ()=>{
-        tester.findElement(driver.By.xpath('//*[@id="cssmenu"]/ul/li[1]/a')).click();
+    it('Accessing SPMB and Click Home button', (done)=>{
+        setTimeout(()=>{
+            tester.findElement(driver.By.xpath('//*[@id="cssmenu"]/ul/li[1]/a')).click();
+            done();
+        },1500)
+    });
+
+    it('Accessing SPMB and Click download at Home page',(done)=>{
+        setTimeout(()=>{
+            tester.get('http://spmb.polinema.ac.id/home')
+            tester.findElement(driver.By.xpath('//*[@id="content-in"]/p[1]/a')).click();
+            done();
+        },1500)
+    });
+
+    it('Accessing SPMB and Click Magister (klik disini)',(done)=>{
+        setTimeout(()=>{
+            tester.get('http://spmb.polinema.ac.id/home')
+            tester.findElement(driver.By.xpath('//*[@id="content-in"]/ul/li[1]/a')).click();
+            done();
+        },1500)
+    });
+
+    it('Accessing SPMB and Click prodi in navbar',(done)=>{
+        setTimeout(()=>{
+            tester.findElement(driver.By.xpath('//*[@id="cssmenu"]/ul/li[2]/a')).click();
+            done();
+        },1500)
+    });
+
+    it ('Accessing panitia login',(done)=>{
+        setTimeout(()=>{
+            tester.findElement(driver.By.xpath('//*[@id="panitia"]/a')).click();
+            done()
+        },1500)
     });
 });
